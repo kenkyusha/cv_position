36b8cbf74ab9668d6a28cabdcf53cb9b774a3008
Arne Niitsoo <arneniitsoo@gmail.com>
2019-05-25 17:16:07 +0200

    setupstuff

---
+++ setupstuff.sh
@@ -0,0 +1,13 @@
+# creates folders etc
+# RUN THIS to setup stuff:
+# chmod +x setupstuff.sh
+mkdir data
+cd data
+wget https://www2.iis.fraunhofer.de/IPIN/testing/cross.tar.gz
+wget https://www2.iis.fraunhofer.de/IPIN/training/horizontal.tar.gz
+wget https://www2.iis.fraunhofer.de/IPIN/training/vertical.tar.gz
+tar -xvzf cross.tar.gz
+tar -xvzf horizontal.tar.gz
+tar -xvzf vertical.tar.gz
+
+#chmod +x setupstuff.sh
